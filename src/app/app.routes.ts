befbe3d1a28c78d7008f33e8f2d139405fc1d1cb
foxcaulfield <fox.caulfield@gmail.com>
2024-07-03 13:13:40 +0200

    Add component, update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PostsPageComponent } from './posts/posts-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "posts", component: PostsPageComponent}
+];
---
+++ src/app/posts/posts-page.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-posts-page',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      posts-page works!
+    </p>
+  `,
+  styles: ``
+})
+export class PostsPageComponent {
+
+}
