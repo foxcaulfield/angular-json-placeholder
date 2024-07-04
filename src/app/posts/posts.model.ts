561a5f11ac747d89bc51a13d4b5f8300ca093f40
foxcaulfield <fox.caulfield@gmail.com>
2024-07-04 17:31:10 +0200

    Update posts model

--- src/app/posts/posts.model.ts
+++ src/app/posts/posts.model.ts
@@ -1,15 +1,35 @@
+/* https://jsonplaceholder.typicode.com */
+// export interface ExternalPostModel {
+//     userId: number;
+//     id: number;
+//     title: string;
+//     body: string;
+// }
+
 export interface ExternalPostModel {
-    userId: number;
     id: number;
     title: string;
     body: string;
+    tags: string[];
+    reactions: {
+        likes: number;
+        dislikes: number;
+    };
+    views: number;
+    userId: number;
 }
 
 export interface PostsModel {
-    userId: string;
-    id: string;
+    id: number;
     title: string;
     body: string;
+    tags: string[];
+    reactions: {
+        likes: number;
+        dislikes: number;
+    };
+    views: number;
+    userId: number;
 }
 
 export interface PostCreateDto extends Omit<PostsModel, "id"> {}
